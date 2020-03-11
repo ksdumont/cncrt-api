d7b0926b832b708c9364bbf64f1272b1c5584e72
ksdumont <ksdumont@gmail.com>
2020-03-11 08:59:27 -0700

    2 routes added

--- src/artists/artists-router.js
+++ src/artists/artists-router.js
@@ -1,7 +1,68 @@
-const express = require('express')
-const ArtistsService = require('./artists-service')
-const artistsRouter = express.Router()
-const jsonParser = express.json()
+const express = require("express");
+const path = require("path");
+const ArtistsService = require("./artists-service");
+const artistsRouter = express.Router();
+const jsonParser = express.json();
 
 artistsRouter
-.route('/')
+  .route("/")
+  .get((req, res, next) => {
+    ArtistsService.getAllArtists(req.app.get("db"))
+      .then(artists => {
+        res.json(artists);
+      })
+      .catch(next);
+  })
+  .post(jsonParser, (req, res, next) => {
+    const { name, email, username, password } = req.body;
+    const newArtist = {
+      name,
+      email,
+      username,
+      password
+    };
+
+    for (const [key, value] of Object.entries(newArtist)) {
+      if (value == null) {
+        return res.status(400).json({
+          error: {
+            message: `Missing '${key}' in request body`
+          }
+        });
+      }
+    }
+    ArtistsService.insertArtist(req.app.get("db"), newArtist)
+      .then(artist => {
+        res
+          .status(201)
+          .location(path.posix.join(req.originalUrl, `/${artist.id}`))
+          .json(artist);
+      })
+      .catch(next);
+  });
+artistsRouter
+  .route("/:id")
+  .all((req, res, next) => {
+    ArtistsService.getById(req.app.get("db"), req.params.id)
+      .then(artist => {
+        if (!artist) {
+          return res
+            .status(404)
+            .json({ error: { message: `Artist does not exist` } });
+        }
+        res.artist = artist;
+        next();
+      })
+      .catch(next);
+  })
+  .get()
+  .post()
+  .delete((req, res, next) => {
+    ArtistsService.deleteArtist(req.app.get("db"), req.params.id)
+      .then(() => {
+        res.status(204).end();
+      })
+      .catch(next);
+  });
+
+module.exports = artistsRouter;
--- src/artists/artists-service.js
+++ src/artists/artists-service.js
@@ -1,4 +1,32 @@
 const ArtistsService = {
-
-}
+  getAllArtists(knex) {
+    return knex.select("*").from("artists");
+  },
+  insertArtist(knex, newArtist) {
+    return knex
+      .insert(newArtist)
+      .into("artists")
+      .returning("*")
+      .then(rows => {
+        return rows[0];
+      });
+  },
+  updateArtist(knex, id, newFields) {
+    return knex("artists")
+      .where({ id })
+      .update(newFields);
+  },
+  getById(knex, id) {
+    return knex
+      .from("artists")
+      .select("*")
+      .where("id", id)
+      .first();
+  },
+  deleteArtist(knex, id) {
+    return knex("artists")
+      .where({ id })
+      .delete();
+  }
+};
 module.exports = ArtistsService;
