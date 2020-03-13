d1560664baecaf1afd1a3c5d1cf260bed1116c07
ksdumont <ksdumont@gmail.com>
2020-03-13 13:32:08 -0700

    post deploy script

--- src/config.js
+++ src/config.js
@@ -3,5 +3,7 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || "development",
   DATABASE_URL:
     process.env.DATABASE_URL || "postgresql://dunder_mifflin@localhost/cncrt",
-  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL
+  TEST_DATABASE_URL:
+    process.env.TEST_DATABASE_URL ||
+    "postgresql://dunder_mifflin@localhost/cncrttest"
 };
